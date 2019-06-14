ddd7ad46c41346fa2126e0720ac09f3a72a177fb
Boots2291 <buteramv@gmail.com>
2019-06-14 15:05:09 -0400

    Add phonebook part 3: add numbers to person object

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,50 +2,60 @@ import React, { useState } from 'react'
 import Person from './components/Person'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas' }
-    ])
-    const [newName, setNewName] = useState('')
-    
-    const rows = () => persons.map(person => <Person key={person.name} person={person} />)
-    
-    const handleNameInputChange = (e) => {
-        setNewName(e.target.value)
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '555-555-5555' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const rows = () => persons.map(person => <Person key={person.name} person={person} />)
+
+  const handleNameInputChange = (e) => {
+    setNewName(e.target.value)
+  }
+
+  const handleNumberInputChange = (e) => {
+    setNewNumber(e.target.value)
+  }
+
+  const addPerson = (e) => {
+    e.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber,
     }
 
-    const addPerson = (e) => {
-        e.preventDefault()
-        const personObject = {
-            name: newName,
-        }
-
-        if (persons.findIndex(person => person.name === newName) === -1) {
-            setPersons(persons.concat(personObject))
-            setNewName('')
-            clearForm()
-        } else {
-            alert(`${newName} is already added to the phonebook`)
-            clearForm()
-        }
+    if (persons.findIndex(person => person.name === newName) === -1) {
+      setPersons(persons.concat(personObject))
+      setNewName('')
+      setNewNumber('')
+      clearForm()
+    } else {
+      alert(`${newName} is already added to the phonebook`)
+      clearForm()
     }
-    
-    const clearForm = () => document.getElementById('addToPhonebookForm').reset()
+  }
 
-    return (
+  const clearForm = () => document.getElementById('addToPhonebookForm').reset()
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson} id="addToPhonebookForm">
         <div>
-            <h2>Phonebook</h2>
-            <form onSubmit={addPerson} id="addToPhonebookForm">
-                <div>
-                    name: <input onChange={handleNameInputChange} />
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
-            </form>
-            <h2>Numbers</h2>
-            {rows()}
+          name: <input onChange={handleNameInputChange} />
         </div>
-    )
+        <div>
+          number: <input onChange={handleNumberInputChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {rows()}
+    </div>
+  )
 }
 
 export default App
